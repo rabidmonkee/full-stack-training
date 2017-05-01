d823d2e8f2ac6773f64bf66e88a2241c40901fff
Brad J Garner <bjgarner@emarketinginc.com>
2017-05-01 13:51:18 -0700

    finished porfolio site project

--- resumeviewer/src/App.js
+++ resumeviewer/src/App.js
@@ -1,27 +1,53 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import './App.css';
 import Header from './Components/Header';
 import About from './Components/About';
 import Resume from './Components/Resume';
 import Portfolio from './Components/Portfolio';
-import Testimonials from './Components/Testimonials';
 import Contact from './Components/Contact';
 import Footer from './Components/Footer';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-		  <Header />
-		  <About />
-		  <Resume />
-		  <Portfolio />
-		  <Testimonials />
-		  <Contact />
-		  <Footer />
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			resumeData:{}
+		}
+	}
+
+	getResumeData(){
+		$.ajax({
+			url: 'http://localhost:3000/resumeData.json',
+			dataType: 'json',
+			cache: false,
+			success: function(data){
+				this.setState({resumeData: data})
+			}.bind(this),
+			error: function(xhr, status, err){
+				console.log(err);
+				alert(err);
+			}
+		})
+	}
+
+	componentDidMount(){
+		this.getResumeData();
+	}
+
+	render() {
+
+		return (
+			<div className="App">
+				<Header data={this.state.resumeData.main} />
+				<About data={this.state.resumeData.main} />
+				<Resume data={this.state.resumeData.resume} />
+				<Portfolio data={this.state.resumeData.portfolio} />
+				<Contact data={this.state.resumeData.main} />
+				<Footer data={this.state.resumeData.main} />
+			</div>
+		);
+	}
 }
 
 export default App;
--- resumeviewer/src/Components/Footer.js
+++ resumeviewer/src/Components/Footer.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 class Footer extends Component {
   render() {
+	  if(this.props.data){
+  		var networks = this.props.data.social.map(function (network){
+  			return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+  		});
+  	}
     return (
 		<div>
 			<footer>
@@ -11,18 +16,12 @@ class Footer extends Component {
 					<div className="twelve columns">
 
 						<ul className="social-links">
-							<li><a href="#"><i className="fa fa-facebook"></i></a></li>
-							<li><a href="#"><i className="fa fa-twitter"></i></a></li>
-							<li><a href="#"><i className="fa fa-google-plus"></i></a></li>
-							<li><a href="#"><i className="fa fa-linkedin"></i></a></li>
-							<li><a href="#"><i className="fa fa-instagram"></i></a></li>
-							<li><a href="#"><i className="fa fa-dribbble"></i></a></li>
-							<li><a href="#"><i className="fa fa-skype"></i></a></li>
+							{networks}
 						</ul>
 
 						<ul className="copyright">
 							<li>&copy; Copyright 2014 CeeVee</li>
-							<li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>   
+							<li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
 						</ul>
 
 					</div>
